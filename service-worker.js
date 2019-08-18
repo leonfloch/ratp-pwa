6b03a4eaf924fe5196b37076022764ea03215a4a
Leonardo Valbuena <leov.calderon@gmail.com>
2019-08-18 16:47:01 -0500

    add data cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const CACHE_NAME = 'static-cache-v10';
+const CACHE_NAME = 'static-cache-v11';
+const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
-    '/offline.html',    
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/scripts/app.js',
+    '/styles/inline.css',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg'
 ];
 
 
@@ -24,7 +31,7 @@ self.addEventListener('activate', (evt) => {
     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
-                if (key !== CACHE_NAME) {
+                if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
@@ -37,20 +44,33 @@ self.addEventListener('activate', (evt) => {
 
 
 self.addEventListener('fetch', (evt) => {
-
-    if (evt.request.mode !== 'navigate') {
-        // Not a page navigation, bail.
+    if (evt.request.url.includes('/schedules/')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then((cache) => {
+                return fetch(evt.request)
+                    .then((response) => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    }).catch((err) => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }));
         return;
     }
     evt.respondWith(
-        fetch(evt.request)
-            .catch(() => {
-                return caches.open(CACHE_NAME)
-                    .then((cache) => {
-                        return cache.match('offline.html');
-                    });
-            })
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(evt.request)
+                .then((response) => {
+                    return response || fetch(evt.request);
+                });
+        })
     );
+
 
 
 
